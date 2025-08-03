17c581cb8bba5d53db31b36b7dde74c90ea72ae8
macrespo42 <macrespo@student.42.fr>
2025-08-03 20:17:22 +0200

    feat: add a list command

---
+++ cmd/list.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2025 NAME HERE <maxime.crespo@protonmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list all expenses",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		expenses, err := loadExpenses("expenses.json")
+		if err != nil {
+			log.Fatal("Can't load expenses file")
+		}
+
+		fmt.Printf("# %-3s %-10s %-20s %8s\n", "ID", "Date", "Description", "Amount")
+		for _, expense := range expenses {
+			fmt.Printf("  %-3d %-10s %-20s %7.2f$\n", expense.ID, expense.Date.Format("2006-01-02"), expense.Description, expense.Amount)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
